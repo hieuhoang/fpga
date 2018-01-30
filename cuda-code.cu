772298a01f21859d62b4c459d4c7859ac245299f
Hieu Hoang <hieuhoang@gmail.com>
2018-01-30 15:24:57 +0000

    cuda working

--- cuda-code.cu
+++ cuda-code.cu
@@ -7,6 +7,15 @@
 
 using namespace std;
 
+void HandleError(cudaError_t err, const char *file, int line ) {
+  if (err != cudaSuccess) {
+    std::cerr << "ERROR: " << cudaGetErrorString(err) << " in " << file << " at line " << line << std::endl;
+    exit( EXIT_FAILURE );
+  }
+}
+
+#define HANDLE_ERROR( err ) (HandleError( err, __FILE__, __LINE__ ))
+
 void HandleErrorCublas(cublasStatus_t err, const char *file, int line ) {
   if (err != CUBLAS_STATUS_SUCCESS) {
     std::cerr << "CUBLAS ERROR: " << err << " in " << file << " at line " << line << std::endl;
@@ -29,26 +38,30 @@ void runCuda()
   cublasOperation_t opA = CUBLAS_OP_N;
   cublasOperation_t opB = CUBLAS_OP_N;
 
-  int m = 7;
-  int n = 7;
-  int k = 7;
+  int m = 85000;
+  int n = 640;
+  int k = 512;
 
-  int lda = 4;
-  int ldb = 6;
-  int ldc = 7;
+  int lda = 85000;
+  int ldb = 512;
+  int ldc = 85000;
 
-  const float *alpha = NULL;
-  const float *beta = NULL;
-  const float *A = NULL;
-  const float *B = NULL;
-  float *C = NULL;
+  const float alpha = 1;
+  const float beta = 0;
+  const float *A;
+  const float *B;
+  float *C;
 
-  HANDLE_ERROR_CUBLAS(cublasSgemm(handle, opB, opA,
-                      n, m, k,
-                      alpha,
-                      B, ldb,
+  HANDLE_ERROR( cudaMalloc(&A, 85000 * 512 * sizeof(float)) );
+  HANDLE_ERROR( cudaMalloc(&B, 512 * 640 * sizeof(float)) );
+  HANDLE_ERROR( cudaMalloc(&C, 85000 * 640 * sizeof(float)) );
+
+  HANDLE_ERROR_CUBLAS(cublasSgemm(handle, opA, opB,
+                      m, n, k,
+                      &alpha,
                       A, lda,
-                      beta,
+                      B, ldb,
+                      &beta,
                       C, ldc));
 
 }
